4fbcf6d064f4e978c0d0837a78d75b154c37b321
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-06-27 11:27:59 -0300

    tentando adicionar produto

--- product1.js
+++ product1.js
@@ -25,7 +25,33 @@ function mostrar(pergunta) {
   }
 }
 
-function adicionarAoCarrinhoLateral(nomeProduto, imgProduto){
-  let lista = document.getElementById('sideBar')
+function COMPRAR(){
+  let nome = document.getElementById("produto-card").textContent;
+
+  let imagem = document.getElementById("img-produto").scr;
+
+  let preco = document.getElementById("preco-card").textContent;
+
+  adicionarAoCarrinhoLateral(nome, imagem, preco);
+}
+
+function adicionarAoCarrinhoLateral(nome, imagem, preco){
+  let container = document.getElementById("iten-carrinho");
+
+  let item = document.createElement("div");
+  item.classList.add("iten-lateral");
+
+  let img = document.createElement("img")
+  img.src = imagem;
+  img.alt = nome;
+
+  let texto = document.createElement("div");
+  texto.classList.add("texto-item");
+  texto.innerHTML = `<strong>${nome}</strong><br><span>${preco}</span>`;
+
+  item.appendChild(img);
+  item.appendChild(texto);
+
+  container.appendChild(item);
 }
 
